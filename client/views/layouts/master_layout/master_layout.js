991819826c34146f3db31e3661e8c4ffa15cb1bb
rbaertsch <baertsch@soe.ucsc.edu>
2014-10-14 17:19:43 -0700

    add common header to template

--- client/views/layouts/master_layout/master_layout.js
+++ client/views/layouts/master_layout/master_layout.js
@@ -3,21 +3,30 @@
 /* MasterLayout: Event Handlers and Helpersss .js*/
 /*****************************************************************************/
 Template.MasterLayout.events({
-  /*
-   * Example:
-   *  'click .selector': function (e, tmpl) {
-   *
-   *  }
-   */
+  'change #study': function(event,template){
+	  Session.set('studyID', event.currentTarget.value);
+	  this.currStudy = event.currentTarget.value;
+	  console.log('Session studyID:', Session.get('studyID'));
+	},
+    'click #job': function(event,template){
+  	  //Session.set('studyID', event.currentTarget.value);
+	  Meteor.call('runshell', 'ls', function(err,response) {
+	  			if(err) {
+	  				Session.set('serverDataResponse', "Error:" + err.reason);
+	  				return;
+	  			}
+	  			Session.set('serverDataResponse', response);
+	  		});
+  	  console.log('shell:');
+  	} 
+  
 });
 
 Template.MasterLayout.helpers({
-  /*
-   * Example:
-   *  items: function () {
-   *    return Items.find();
-   *  }
-   */
+	studies: function(){
+		return Studies.find({},{sort: {short_name:1}});
+	}
+  
 });
 
 /*****************************************************************************/
@@ -27,6 +36,9 @@ Template.MasterLayout.created = function () {
 };
 
 Template.MasterLayout.rendered = function () {
+	$(function() {
+	 $( "#tabs" ).tabs();
+	});
 };
 
 Template.MasterLayout.destroyed = function () {
