507e19dff25fea8197dc7de22dca02f7e3094401
chrisw <chrisw@soe.ucsc.edu>
2014-10-17 08:46:45 -0700

    initial commit of observation deck package

--- client/views/clinical_events/clinical_events_index/clinical_events_index.js
+++ client/views/clinical_events/clinical_events_index/clinical_events_index.js
@@ -1,34 +1,68 @@
+// example: https://meteor.hackpad.com/Meteor-Cookbook-Reactive-D3-Visualizations-YUR9JT4mrm9
+// example: http://blog.benmcmahen.com/post/41124327100/using-d3-and-meteor-to-generate-scalable-vector
+
+// https://www.eventedmind.com/feed/meteor-introducing-the-package-system
+// packaging existing libraries http://www.meteorpedia.com/read/Packaging_existing_Libraries#!
+
+// rendered vs Deps.autorun: https://stackoverflow.com/questions/16314505/meteor-template-rendered-why-collection-is-empty
 
 /*****************************************************************************/
 /* ClinicalEventsIndex: Event Handlers and Helpersss .js*/
 /*****************************************************************************/
 Template.ClinicalEventsIndex.events({
-  /*
-   * Example:
-   *  'click .selector': function (e, tmpl) {
-   *
-   *  }
-   */
+    /*
+     * Example:
+     *  'click .selector': function (e, tmpl) {
+     *
+     *  }
+     */
 });
 
 Template.ClinicalEventsIndex.helpers({
-	clinical_events: function(){
-		var study = Session.get('studyID');
-		console.log('get clinical events for '+study);
-		return ClinicalEvents.find({'study_id':study});
-	}
+    clinical_events : function() {
+        console.log('Template.ClinicalEventsIndex.helper.clinical_events');
+        var response = ClinicalEvents.find({});
+
+        var docList = response.fetch();
+        console.log('docList:', docList.length, '<-- from Template.ClinicalEventsIndex.helper.clinical_events');
+
+        return response;
+    }
 });
 
 /*****************************************************************************/
 /* ClinicalEventsIndex: Lifecycle Hooks */
 /*****************************************************************************/
-Template.ClinicalEventsIndex.created = function () {
+Template.ClinicalEventsIndex.created = function() {
 };
 
-Template.ClinicalEventsIndex.rendered = function () {
+Template.ClinicalEventsIndex.rendered = function() {
+    // TODO DOM rendered or changed
+    console.log('Template.ClinicalEventsIndex.rendered');
+
+    var divElem = document.getElementById("test_div");
+
+    // TODO Deps.autorun is triggered when reactive data source has changed
+    Deps.autorun(function() {
+        console.log('Deps.autorun');
+
+        var response = ClinicalEvents.find({});
+
+        var docList = response.fetch();
+        // console.log('docList:', docList.length, ' <-- Deps.autorun');
+        // console.log('docList:', JSON.stringify(docList), ' <-- Deps.autorun');
+
+        if (docList.length > 0) {
+            buildObservationDeck(divElem, {
+                'mongoData' : {
+                    'clinical' : docList
+                }
+            });
+        }
+
+    });
+
 };
 
-Template.ClinicalEventsIndex.destroyed = function () {
+Template.ClinicalEventsIndex.destroyed = function() {
 };
-
-
---
+++ packages/observation-deck/package.js
@@ -0,0 +1,20 @@
+Package.describe({
+    name : 'observation-deck',
+    summary : 'Observation Deck visualizes multiple data types for a group of samples.',
+    version : '1.0.0',
+    git : ''
+});
+
+Package.onUse(function(api) {
+    api.versionsFrom('0.9.0');
+    api.use(['jquery']);
+
+    api.export('buildObservationDeck', 'client');
+    api.addFiles('observation-deck.js', 'client');
+});
+
+Package.onTest(function(api) {
+    api.use('tinytest');
+    api.use('observation-deck');
+    api.addFiles('observation-deck-tests.js');
+});
