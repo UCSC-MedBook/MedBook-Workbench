fc5fbbcb0b51095f419b1ef5d050a5b57281d4d1
rbaertsch <baertsch@soe.ucsc.edu>
2014-10-20 21:53:19 -0700

    add pathway view

---
+++ client/controllers/pathways.js
@@ -0,0 +1,11 @@
+PathwaysController = RouteController.extend({
+  waitOn: function () {
+  },
+
+  data: function () {
+  },
+
+  action: function () {
+    this.render();
+  }
+});
---
+++ client/views/pathways/pathways.js
@@ -0,0 +1,35 @@
+
+/*****************************************************************************/
+/* Pathways: Event Handlers and Helpersss .js*/
+/*****************************************************************************/
+Template.Pathways.events({
+  /*
+   * Example:
+   *  'click .selector': function (e, tmpl) {
+   *
+   *  }
+   */
+});
+
+Template.Pathways.helpers({
+  /*
+   * Example:
+   *  items: function () {
+   *    return Items.find();
+   *  }
+   */
+});
+
+/*****************************************************************************/
+/* Pathways: Lifecycle Hooks */
+/*****************************************************************************/
+Template.Pathways.created = function () {
+};
+
+Template.Pathways.rendered = function () {
+};
+
+Template.Pathways.destroyed = function () {
+};
+
+
