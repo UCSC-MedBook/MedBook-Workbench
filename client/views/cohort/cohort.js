cdd821da0307ba2ac0704f953077ae441ed20c35
rbaertsch <baertsch@soe.ucsc.edu>
2014-10-28 15:04:04 -0700

    add obs deck to cohort tab, add patient tab

--- client/views/cohort/cohort.js
+++ client/views/cohort/cohort.js
@@ -27,6 +27,22 @@ Template.Cohort.created = function () {
 };
 
 Template.Cohort.rendered = function () {
+    var divElem = document.getElementById("Cohort_OD_Div");
+
+    // TODO Deps.autorun is triggered when reactive data source has changed
+    Deps.autorun(function() {
+        // console.log('Deps.autorun');
+
+        //var response = ClinicalEvents.find({});
+        //
+        //var docList = response.fetch();
+        //
+        //console.log('docList.length:', docList.length, ' <-- Deps.autorun');
+        //console.log('docList:', JSON.stringify(docList), ' <-- Deps.autorun');
+        //
+        buildObservationDeck(divElem, null);
+		
+	});
 };
 
 Template.Cohort.destroyed = function () {
