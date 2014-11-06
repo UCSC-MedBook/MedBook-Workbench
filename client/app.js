f5a60bd909f5934100b2089a5e6831eff3ae5155
rbaertsch <baertsch@soe.ucsc.edu>
2014-11-05 17:06:02 -0800

    fix bug

--- client/app.js
+++ client/app.js
@@ -14,12 +14,3 @@ App.helpers = {
 _.each(App.helpers, function (helper, key) {
   Handlebars.registerHelper(key, helper);
 });
-
-
-var blobStore = new FS.Store.GridFS("blobs", {
-  mongoUrl: 'mongodb://127.0.0.1:27017/MedBook/', // optional, defaults to Meteor's local MongoDB
-  mongoOptions: {...},  // optional, see note below
-  maxTries: 1, // optional, default 5
-  chunkSize: 1024*1024  // optional, default GridFS chunk size in bytes (can be overridden per file).
-                        // Default: 2MB. Reasonable range: 512KB - 4MB
-});
--- server/collections/blobs.js
+++ server/collections/blobs.js
@@ -5,38 +5,3 @@
  *  }
  */
 
-Blobs.allow({
-  insert: function (userId, doc) {
-    return true;
-  },
-
-  read: function (userId, doc, fieldNames, modifier) {
-    return true;
-  },
-
-  write: function (userId, doc, fieldNames, modifier) {
-    return true;
-  },
-
-  remove: function (userId, doc) {
-    return true;
-  }
-});
-
-Blobs.deny({
-  insert: function (userId, doc) {
-    return false;
-  },
-  
-  read: function (userId, doc, fieldNames, modifier) {
-    return false;
-  },
-
-  write: function (userId, doc, fieldNames, modifier) {
-    return false;
-  },
-
-  remove: function (userId, doc) {
-    return false;
-  }
-});
