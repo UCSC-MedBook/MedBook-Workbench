ca3f9177b40f43615b7e407ffc665eeefe9bd661
rbaertsch <baertsch@soe.ucsc.edu>
2015-01-18 14:03:59 -0800

    add error message reading config.toml

--- client/controllers/cohort.js
+++ client/controllers/cohort.js
@@ -5,6 +5,7 @@ CohortController = RouteController.extend({
 		//var geneList = ['PEG10', 'KCNJ6', 'FGF9', 'CNKSR3', 'ANK2', 'ST8SIA4', 'RUNX1T1', 'GPRIN2', 'KIT', 'GABRB3', 'IPCEF1', 'GRIN3A', 'CACHD1', 'GYG2', 'ADM', 'F2RL1', 'TMPRSS2', 'TEAD2', 'DHODH', 'FXYD3', 'SERTAD1', 'NQO1', 'DHCR24', 'BANK1', 'INO80C', 'SLC30A4', 'F5', 'HK2', 'PPARG', 'CXCL2', 'FGFRL1', 'NNMT', 'PFKFB4', 'PRR5', 'SPINK1', 'OPHN1', 'KLRB1', 'ERP27', 'SELL', 'IRAK2', 'APOH', 'HSH2D', 'REEP6', 'KLK3', 'MAFK', 'ATP2C2', 'AGR2', 'ACOT9', 'ANG', 'CEACAM1'];
 		Meteor.subscribe('clinical_events_index');
         Meteor.subscribe('expression', geneList);
+		console.log('subscribe expression ', geneList)
     },
 
     data : function() {
