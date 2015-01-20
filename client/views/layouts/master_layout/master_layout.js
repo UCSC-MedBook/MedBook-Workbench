b391a818a3d2f1a7b705c7af8f3921da2ffd2ec7
chrisw <chrisw@soe.ucsc.edu>
2015-01-20 14:06:42 -0800

    nuke button also clears a cookie

--- client/views/layouts/master_layout/master_layout.js
+++ client/views/layouts/master_layout/master_layout.js
@@ -1,41 +1,43 @@
-
 /*****************************************************************************/
 /* MasterLayout: Event Handlers and Helpersss .js*/
 /*****************************************************************************/
 Template.MasterLayout.events({
-	'change #study': function(event,template){
-		Session.set('studyID', event.currentTarget.value);
-		this.currStudy = event.currentTarget.value;
-		console.log('Session studyID:', Session.get('studyID'));
-	},
-	'click .nuc' : function(){
-		location.reload(true);
-	}
+    'change #study' : function(event, template) {
+        Session.set('studyID', event.currentTarget.value);
+        this.currStudy = event.currentTarget.value;
+        console.log('Session studyID:', Session.get('studyID'));
+    },
+    'click .nuc' : function() {
+        document.cookie = 'od_config={};path=/';
+        location.reload(true);
+    }
 });
 
 Template.MasterLayout.helpers({
-	studies: function(){
-		return Studies.find({},{sort: {short_name:1}});
-	},
-	selected: function(){
-		if (Session.get('studyID') == this._id) 
-			return true;
-		else 
-			return false;
-	}
-  
+    studies : function() {
+        return Studies.find({}, {
+            sort : {
+                short_name : 1
+            }
+        });
+    },
+    selected : function() {
+        if (Session.get('studyID') == this._id)
+            return true;
+        else
+            return false;
+    }
 });
 
 /*****************************************************************************/
 /* MasterLayout: Lifecycle Hooks */
 /*****************************************************************************/
-Template.MasterLayout.created = function () {
+Template.MasterLayout.created = function() {
 };
 
-Template.MasterLayout.rendered = function () {
+Template.MasterLayout.rendered = function() {
 };
 
-Template.MasterLayout.destroyed = function () {
+Template.MasterLayout.destroyed = function() {
 };
-
 
