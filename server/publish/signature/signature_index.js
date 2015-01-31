2cdd32bae97b6497c1919204a5c4eb5d184e0146
chrisw <chrisw@soe.ucsc.edu>
2015-01-30 21:44:56 -0800

    Improve server console output for debugging. Remove extraneous find().

--- server/publish/signature/signature_index.js
+++ server/publish/signature/signature_index.js
@@ -1,21 +1,36 @@
 /*****************************************************************************/
 /* SignatureIndex Publish Functions
-/*****************************************************************************/
+ /*****************************************************************************/
 
-Meteor.publish('signature_index', function (study) {
-  // you can remove this if you return a cursor
-	var sg = null;
-	var cnt = 0;
-	try {
-		sg = Signature.find({'studyID':study})
-		cnt = Signature.find({'studyID':study}).count()
-		 console.log('Signature count for study', study,  cnt);
-	}
-	catch(error) {
-		sg = Signature.find();
- 	    cnt = Signature.find().count();
-	}
-  console.log('Signature count', cnt);
-  return sg;
+Meteor.publish('signature_index', function(study) {
+    // you can remove this if you return a cursor
+
+    study = 'prad_wcdt';
+
+    var findResult = Signature.find({
+        'studyID' : study
+    });
+
+    var count = findResult.count();
+    console.log('signature_index count', count, '<-- publish signature_index for:', study);
+
+    return findResult;
+
+    // var sg = null;
+    // var cnt = 0;
+    // try {
+    // sg = Signature.find({
+    // 'studyID' : study
+    // });
+    // cnt = Signature.find({
+    // 'studyID' : study
+    // }).count();
+    // console.log('Signature count for study', study, cnt);
+    // } catch(error) {
+    // sg = Signature.find();
+    // cnt = Signature.find().count();
+    // }
+    // console.log('Signature count', cnt);
+    // return sg;
 });
 
