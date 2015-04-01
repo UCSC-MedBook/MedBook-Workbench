35d0b5f3a7616ba16554b724e074724270fd0723
rbaertsch <baertsch@soe.ucsc.edu>
2015-03-31 18:50:33 -0700

    missing file

---
+++ both/collections/expression2.js
@@ -0,0 +1,7 @@
+Expression2 = new Mongo.Collection('expression_import');
+/*
+ * Add query methods like this:
+ *  Expression2.findPublic = function () {
+ *    return Expression2.find({is_public: true});
+ *  }
+ */
