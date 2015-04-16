96bd48b6ecc0e9cb256c38f2be73e31540bc0224
chrisw <chrisw@soe.ucsc.edu>
2015-04-16 10:51:34 -0700

    fix bug with publish/subscribe fro expression2 data

--- client/app.js
+++ client/app.js
@@ -6,31 +6,30 @@ _.extend(App, {
 });
 
 App.helpers = {
-	selectedStudy: function () {
-		return Session.get('studyID');
-	}
+    selectedStudy : function() {
+        return Session.get('studyID');
+    }
 };
 
-_.each(App.helpers, function (helper, key) {
-  Handlebars.registerHelper(key, helper);
+_.each(App.helpers, function(helper, key) {
+    Handlebars.registerHelper(key, helper);
 });
-Meteor.startup(function () {
-	console.log('app.js startup');
-	Meteor.subscribe('study_groups_index');
-	Meteor.subscribe('clinical_events_index');
-	Meteor.subscribe('contrast');	
-	Meteor.subscribe('images');
-	Meteor.subscribe('blobs');
-	Meteor.subscribe('signature_index');
-	Meteor.subscribe('signature_scores_index');
-	Meteor.subscribe('results');
-	Meteor.subscribe('expression2');
-	Meteor.subscribe('expression3');
-	Meteor.subscribe('gene_sets_index');
-	Meteor.subscribe('genes');
-	Meteor.subscribe('studies');
-	
-	//Meteor.subscribe('signature_index');
-	Session.set('selectedContrast','')
-	console.log('startup complete')
+Meteor.startup(function() {
+    console.log('app.js startup begin');
+    Meteor.subscribe('study_groups_index');
+    Meteor.subscribe('clinical_events_index');
+    Meteor.subscribe('contrast');
+    Meteor.subscribe('images');
+    Meteor.subscribe('blobs');
+    Meteor.subscribe('signature_index');
+    Meteor.subscribe('signature_scores_index');
+    Meteor.subscribe('results');
+    // Meteor.subscribe('expression2');
+    Meteor.subscribe('gene_sets_index');
+    Meteor.subscribe('genes');
+    Meteor.subscribe('studies');
+
+    //Meteor.subscribe('signature_index');
+    Session.set('selectedContrast', '');
+    console.log('app.js startup complete');
 });
