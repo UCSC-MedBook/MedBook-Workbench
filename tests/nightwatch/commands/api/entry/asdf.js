13ff2c4bc34996d5cd5695b9381fe42629da2d02
Robert Baertsch <baertsch@soe.ucsc.edu>
2015-12-12 13:31:11 -0800

    add tests

---
+++ tests/nightwatch/commands/api/entry/asdf.js
@@ -0,0 +1,5 @@
+exports.command = function() {
+  console.log("asdfasdf")
+
+  return this; // allows the command to be chained.
+};
---
+++ tests/nightwatch/walkthroughs/appLayout.js
@@ -0,0 +1,32 @@
+// add tests to this file using the Nightwatch.js API
+// http://nightwatchjs.org/api
+
+module.exports = {
+  "Upload some BD2KGeneExpression files": function (client) {
+    client
+      .url("http://localhost:3000/wb")
+      .resizeWindow(1024, 768).pause(1000)
+      .reviewMainLayout()
+    ;
+
+    // make sure user exists and log in
+    client
+      .timeoutsAsyncScript(1000)
+      .executeAsync(function(data, done){
+        Accounts.createUser({
+          email: 'testing@medbook.ucsc.edu',
+          password: 'testing',
+          profile: {
+            collaborations: ['testing']
+          }
+        }, done);
+      })
+      .executeAsync(function(data, done) {
+        Meteor.logout(done);
+      })
+      .signIn("testing@medbook.ucsc.edu", "testing")
+    ;
+
+    client.end();
+  }
+};
