d9b06e725fe12dd785573f7dba0a7d6272e3d1d7
chrisw <chrisw@soe.ucsc.edu>
2016-02-02 11:17:31 -0800

    pull select2genelist widget out as its own package

--- client/views/cohort/cohort.js
+++ client/views/cohort/cohort.js
@@ -102,33 +102,33 @@ Template.Cohort.created = function() {
 
 Template.Cohort.rendered = function() {
 
-    var options = {
-        placeholder : "list of genes",
-        url : "/genes",
-        // http://localhost:3000/genes?q=MDM
-        // response looks like this: {"items":[{"id":"MDM1","text":"MDM1"},{"id":"MDM2","text":"MDM2"},{"id":"MDM4","text":"MDM4"}]}
-        // url : "https://api.github.com/search/repositories",
-        minimumInputLength : 2,
-        multiple : true,
-        changeEventCallback : function(value) {
-            var sessionVar = "cohort_tab_genelist_widget";
-            var sessionGeneList = Session.get(sessionVar);
-            sessionGeneList = (_.isUndefined(sessionGeneList)) ? [] : sessionGeneList;
-
-            var changes = {
-                added : [],
-                deleted : []
-            };
-
-            changes.added = _.difference(value, sessionGeneList);
-            changes.deleted = _.difference(sessionGeneList, value);
-
-            Session.set(sessionVar, value);
-        }
-    };
-
-    // geneListWidget is exposed via select2genelist package
-    geneListWidget.setupWidget("#selectTagForSelect2", options);
+    // var options = {
+        // placeholder : "list of genes",
+        // url : "/genes",
+        // // http://localhost:3000/genes?q=MDM
+        // // response looks like this: {"items":[{"id":"MDM1","text":"MDM1"},{"id":"MDM2","text":"MDM2"},{"id":"MDM4","text":"MDM4"}]}
+        // // url : "https://api.github.com/search/repositories",
+        // minimumInputLength : 2,
+        // multiple : true,
+        // changeEventCallback : function(value) {
+            // var sessionVar = "cohort_tab_genelist_widget";
+            // var sessionGeneList = Session.get(sessionVar);
+            // sessionGeneList = (_.isUndefined(sessionGeneList)) ? [] : sessionGeneList;
+//
+            // var changes = {
+                // added : [],
+                // deleted : []
+            // };
+//
+            // changes.added = _.difference(value, sessionGeneList);
+            // changes.deleted = _.difference(sessionGeneList, value);
+//
+            // Session.set(sessionVar, value);
+        // }
+    // };
+//
+    // // geneListWidget is exposed via select2genelist package
+    // geneListWidget.setupWidget("#selectTagForSelect2", options);
 
     // $(".select2-search__field");
 
