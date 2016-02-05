aeac4ddbe0e62844c8c3126b93a41602187068cf
chrisw <chrisw@soe.ucsc.edu>
2016-02-04 16:24:11 -0800

    move study and contrast selection controls to medbook:study-contrast-selector

--- client/views/contrast/contrast.js
+++ client/views/contrast/contrast.js
@@ -2,33 +2,33 @@
 /*****************************************************************************/
 /* Contrast: Event Handlers and Helpersss .js*/
 /*****************************************************************************/
-Template.Contrast.events({
-  	'change #contrast': function(event,template){
-	  Session.set('selectedContrast', event.currentTarget.value);
-	  this.currContrast = event.currentTarget.value;
-	  console.log('Session contrastID:', Session.get('selectedContrast'));
-	}, 
- 
-});
-
-Template.Contrast.helpers({
-	contrast: function(){
-		var study = Session.get('studyID') ;
-		console.log('find contrasts for studyID:'+study);
-		return Contrast.find({"studyID":study},{sort: {name:1}});
-	},
-});
+// Template.Contrast.events({
+  	// 'change #contrast': function(event,template){
+	  // Session.set('selectedContrast', event.currentTarget.value);
+	  // this.currContrast = event.currentTarget.value;
+	  // console.log('Session contrastID:', Session.get('selectedContrast'));
+	// },
+//
+// });
+//
+// Template.Contrast.helpers({
+	// contrast: function(){
+		// var study = Session.get('studyID') ;
+		// console.log('find contrasts for studyID:'+study);
+		// return Contrast.find({"studyID":study},{sort: {name:1}});
+	// },
+// });
 
 /*****************************************************************************/
 /* Contrast: Lifecycle Hooks */
 /*****************************************************************************/
-Template.Contrast.created = function () {
-};
-
-Template.Contrast.rendered = function () {
-};
-
-Template.Contrast.destroyed = function () {
-};
+// Template.Contrast.created = function () {
+// };
+//
+// Template.Contrast.rendered = function () {
+// };
+//
+// Template.Contrast.destroyed = function () {
+// };
 
 
--- client/views/layouts/master_layout/master_layout.js
+++ client/views/layouts/master_layout/master_layout.js
@@ -2,30 +2,6 @@
 /* MasterLayout: Event Handlers and Helpersss .js*/
 /*****************************************************************************/
 Template.MasterLayout.events({
-    'change #study' : function(event, template) {
-        this.currStudy = event.currentTarget.value;
-		var currentRoute = Router.current().route.getName();
-		console.log('#change study and set session studyID ', currentRoute, this.currStudy);
-		Session.set('studyID', this.currStudy);
-		Router.go(currentRoute,{}, {query: {'study':this.currStudy}});
-		//{'study':event.currentTarget.value}})
-
-        //console.log('Session studyID:', Session.get('studyID'));
-    },
-    'change #contrast1' : function(event, template) {
-	 	this.currContrast = event.target.value;
-		var studyId = Session.get('studyID');
-		console.log('get session studyID', studyId);
-		console.log('#change contrast', event);
-		var currentRoute = Router.current().route.getName();
-		console.log('->contrast route', currentRoute, 'cont', this.currContrast, 'this', this);
-//		if (currentRoute == 'Scv') {
-//			Router.go('Scv',{_id:this.currContrast}, {query: {study:studyId, contrast:this.currContrast}});
-//		}
-//		else {
-//			Router.go(currentRoute,{_id:this.currContrast}, {query: {study:studyId, contrast:this.currContrast}});
-//		}
-	},
     'click .nuc' : function() {
         document.cookie = 'od_config={};path=/';
         location.reload(true);
@@ -43,26 +19,6 @@ Template.MasterLayout.helpers({
 			var collaborations = Meteor.user().profile.collaborations;
 			return collaborations;
 		}
-	},
-	studies: function() {
-		return Studies.find({},{sort: {short_name:1}});
-	},
-	selected: function(){
-		if (Session.get('studyID') == this.id)
-			return true;
-		else
-			return false;
-	},
-	currentQueryString: function() {
-		var study = Session.get('studyID');
-		var contrast = Session.get('selectedContrast');
-		if (study) {
-			if (contrast) {
-				return { study: study, contrast: contrast};
-			}
-			return { study: study };
-		}
-		return ;
 	}
 });
 
